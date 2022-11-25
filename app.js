936bcd6431d6056a7e563e3a35a49bfcd8d61872
Bizet <benoitbizetlogitud@gmail.com>
2022-11-25 19:01:09 +0100

    add parsing things

--- app.js
+++ app.js
@@ -13,7 +13,31 @@ const client = new tmi.Client({
         secure: true
     },
 
-// Lack of the identity tags makes the bot anonymous and able to fetch messages from the channel
-// for reading, supervision, spying, or viewing purposes only
+    // Lack of the identity tags makes the bot anonymous and able to fetch messages from the channel
+    // for reading, supervision, spying, or viewing purposes only
+    identity: {
+        username: `${process.env.TWITCH_USERNAME}`,
+        password: `oauth:${process.env.TWITCH_OAUTH}`
+    },
     channels: [`${process.env.TWITCH_CHANNEL}`]
+});
+
+// Connect to the channel specified using the setings found in the configurations
+// Any error found shall be logged out in the console
+client.connect().catch(console.error);
+
+// We shall pass the parameters which shall be required
+client.on('message', (channel, tags, message, self) => {
+    // Lack of this statement or it's inverse (!self) will make it in active
+    if (!self) return;
+
+    //Start parsing
+    if (message.toLowerCase().includes("boulot")) {
+        client.say(channel,`@${tags.username}, t'as dis boulot attention !`)
+        console.log("IL A DIT BOULOT")
+    }
+
+    
+// This logs out all the messages sent on the channel on the terminal
+
 });
